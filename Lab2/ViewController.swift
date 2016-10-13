171ac32a342fcfe0225af63c413e9c798fa250fd
UŇľytkownik GoŇõńá <Guest@imac-12.local>
2016-10-13 08:20:37 +0200

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Użytkownik Gość on 13.10.2016.
+//  Copyright © 2016 Jaromin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
